969600f2724c226f0194449afb49e7c1941fa4ea
Parker <peekaboocoding@gamil.com>
2024-01-01 15:05:55 +0900

    template 조회

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -12,13 +12,13 @@ with DAG(
 ) as dag:
     bash_t1 = BashOperator(
         task_id="bash_t1",
-        bash_command='echo "data_interval_end: {{data_interval_end}}"',
+        bash_command='echo "data_interval_start : {{data_interval_start}}, data_interval_end: {{data_interval_end}}"',
     )
     bash_t2 = BashOperator( # 객체명 
         task_id="bash_t2",  # task id (node 개념) .. * 객체명과 task 명이 동일해야 찾기 쉬움
         env={
         'START_DATE' : '{{data_interval_start | ds}}',
-        'END_DATE' : '{{data_interval_end}}',
+        'END_DATE' : '{{data_interval_end | ds}}',
         },
         bash_command='echo $START_DATE && echo $END_DATE'
     )
