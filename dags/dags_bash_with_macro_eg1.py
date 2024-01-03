1eb6dce257b8f4e19607501b11d279208fba8bc1
Parker <peekaboocoding@gamil.com>
2024-01-04 02:11:23 +0900

    코드 수정

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -16,7 +16,7 @@ with DAG (
         # ds : yyyy-mm-dd
         # dt : yyyy-mm-ddTHH:MI:SS
         env={'START_DATE':'{{data_interval_start.in_timezone("Asia/Seoul") | ds}}', 
-             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macro.dateutil.relativedelta.relativedelta(days=1)) | ds}}'
+             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'
         }, 
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
