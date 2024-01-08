28d45694fc3e3d1f26a4b833335f513cdfa26d14
Parker <peekaboocoding@gamil.com>
2024-01-08 09:42:43 +0900

    bash, python xcom 사용

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -1,45 +1,50 @@
-from airflow import DAG 
-import datetime
+from airflow import DAG
 import pendulum
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
-
 with DAG(
-    dag_id="dags_bash_python_with_xcom", 
+    dag_id="dags_bash_python_with_xcom",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
-    catchup=False,
+    catchup=False
 ) as dag:
+     
+    # python 에서만 @task 를 사용. 
     @task(task_id='python_push')
     def python_push_xcom():
-        result_dict = {'status':'Good','data':[1,2,3],'options_cnt':100}
+        result_dict = {'status' : 'Good', 'data': [1,2,3], 'options_cnt':100}
         return result_dict
-    bash_pull = BashOperator(
+    
+    # task ids 는 task 간의 관계를 파악하기 위해서라도, 써주는 게 좋을 것 같다.
+    bash_pull=BashOperator(
         task_id='bash_pull',
         env={
             'STATUS':'{{ti.xcom_pull(task_ids="python_push")["status"]}}',
             'DATA':'{{ti.xcom_pull(task_ids="python_push")["data"]}}',
-            'OPTIONS_CNT':'{{ti.xcom_pull(task_ids="python_push")["options_cnt"]}}'
-            },
-    bash_command='echo $STATUS && echo $DATA && $OPTIONS_CNT'
-
+            'OPTIONS_CNT':'{{ti.xcom_pull(task_ids="python_push")["options_cnt"]}}',
+        },
+        # B.O는 bash command를 써야 한다.
+        # 변수 호출을 위한 $도 유념해서 보자 
+        bash_command='echo $STATUS && '
+                     'echo $DATA && '
+                     'echo $OPTIONS_CNT'
     )
     python_push_xcom() >> bash_pull
 
     bash_push = BashOperator(
         task_id='bash_push',
         bash_command='echo PUSH_START '
-                     '{{ti.xcom_push(key="bash_pushed",value=200)}} && '
+                     '{{ti.xcom_push(key="bash_pushed", value=200)}} && '
                      'echo PUSH_COMPLETE'
     )
-    
+
     @task(task_id='python_pull')
     def python_pull_xcom(**kwargs):
         ti = kwargs['ti']
-        status_value = ti.xcom_pull(key='bash_pushed')
-        return_value = ti.xcom_pull(task_ids='bash_push')
-        print('status_value:' + str(status_value))
-        print('return_value:' + return_value)
+        status_value = ti.xcom_pull(key="bash_pushed")
+        return_value = ti.xcom_pull(task_ids="bash_push")
+        print('status_value : ' + str(status_value))
+        print('return_value : ' + return_value)
     
     bash_push >> python_pull_xcom()
