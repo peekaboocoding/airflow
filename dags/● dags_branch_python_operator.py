7f8db443d37d059c089448ad77abc941d1508d10
Parker <peekaboocoding@gamil.com>
2024-01-13 15:34:35 +0900

    수정

---
+++ dags/● dags_branch_python_operator.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+
+with DAG(
+    dag_id='dags_branch_python_operator',
+    start_date=pendulum.datetime(2024,1,1, tz='Asia/Seoul'), 
+    schedule='0 1 * * *',
+    catchup=False
+) as dag:
+    def select_random():
+        import random
+
+        item_list=['A','B','C']
+        selected_item=random.choice(item_list)
+        if selected_item == 'A':
+            return 'task_A'
+        elif select_random in ['B','C'] :
+            return [ 'task_B','task_C']
+        
+    python_branch_task = BranchPythonOperator(
+        task_id='python_branch_task',
+        python_callable=select_random
+    )
+
+    def common_func(**kwargs):
+        print(kwargs['selected'])
+
+    task_a = PythonOperator(
+        task_id='task_A',
+        python_callable=common_func,
+        op_kwargs={'selected':'A'}
+    )
+    task_b = PythonOperator(
+        task_id='task_B',
+        python_callable=common_func,
+        op_kwargs={'selected':'B'}
+    )
+    task_c = PythonOperator(
+        task_id='task_C',
+        python_callable=common_func,
+        op_kwargs={'selected':'C'}
+    )
+    python_branch_task >> [task_a, task_b, task_c]
